d3b6e104178fc57b26ea1fd12459535b991e8549
Emir Tetik <info@frontmir.com>
2023-11-08 19:42:29 +0300

    gereksiz dosyalar kaldırıldı.

--- src/pages/favorite/index.jsx
+++ src/pages/favorite/index.jsx
@@ -3,14 +3,14 @@ import {useDispatch} from 'react-redux'
 import "./favorite.css";
 import {removeFavorite} from '../../store/actions';
 const Favorite = () => {
-  const favorites = useSelector(state => state.favorites);
+  const favoriteState = useSelector(state => state.favorites);
   const dispatch = useDispatch()
 
 
   return (
     <div className='favorite'>
        <ul className='list'>
-        {favorites.map((item, index) => (
+        {favoriteState.map((item, index) => (
           <li key={index}>{item.name} <button className='remove_btn' onClick={() => dispatch(removeFavorite(item))}>remove</button></li>
         ))}
        </ul>
